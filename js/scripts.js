1227a7b1fec37f0ebf42a351a234475f339fe8c9
Matej Kalc <mk9693@student.uni-lj.si>
2022-08-14 00:29:44 +0200

    Removed update on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -11,23 +11,6 @@ window.addEventListener('DOMContentLoaded', event => {
 
     $('#openMessage').modal('show')
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    //navbarShrink();
-
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
